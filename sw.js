da34c14b31f62c26e8e965a59cf19b76dcf634f9
propanfloete7687 <ingoburgold87@gmail.com>
2026-05-31 18:26:42 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,11 @@
 const CACHE = 'zumba-trainer-v1';
-const FILES = ['/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
+const FILES = [
+  '/zumba-trainer/',
+  '/zumba-trainer/index.html',
+  '/zumba-trainer/manifest.json',
+  '/zumba-trainer/icon-192.png',
+  '/zumba-trainer/icon-512.png'
+];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
